3bfd141b4a9c7931e9b3c1302a6056624af5edb6
Josh Lind <lind.98@osu.edu>
2019-10-06 10:09:15 -0400

    Made the layout for the panels page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelsViewComponent } from './components/panels-view/panels-view.component';
+import { VideoFeedComponent } from './components/video-feed/video-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelsViewComponent,
+    VideoFeedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/panels-view/panels-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panels-view',
+  templateUrl: './panels-view.component.html',
+  styleUrls: ['./panels-view.component.css']
+})
+export class PanelsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
